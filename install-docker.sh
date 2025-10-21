5a2ab2f0d92f9a7550e8435d9e89de9a8d12800e
tux-box <nathan.flow@gmail.com>
2025-10-21 16:30:24 -0500

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,6 @@
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker $USER
